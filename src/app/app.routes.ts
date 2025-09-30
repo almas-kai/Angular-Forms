2a1d2ada1f09f0083abe9eeefebc17f48e775b88
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-09-30 21:24:56 +0500

    Simple example for each type of form.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ReactiveForms } from './components/reactive-forms/reactive-forms';
+import { TemplateDrivenForms } from './components/template-driven-forms/template-driven-forms';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: App
+	},
+	{
+		path: 'reactive-forms',
+		component: ReactiveForms
+	},
+	{
+		path: 'template-driven-forms',
+		component: TemplateDrivenForms
+	}
+];
